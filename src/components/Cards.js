e6b276661e84cdd4ed41b508f634474b6addf2f1
Kaleb Litster <kaleb.litster95@gmail.com>
2020-01-12 18:29:16 -0700

    .

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -11,8 +11,12 @@ class Cards extends React.Component{
                     <img className="card-img-top" src={this.props.img} alt="Card image cap"/>
                 </div>
                     <div className="card-body">
+                        <div className="blackline">
+
+                        </div>
                         <h5 className="card-title">{this.props.title}</h5>
-                        <p className="card-text">{this.props.des}</p>
+
+                        {/*<p className="card-text">{this.props.des}</p>*/}
                         <h5>${this.props.price}</h5>
                         <button tag={<Modal/>} to={<Modal/>} > learn </button>
                     </div>
